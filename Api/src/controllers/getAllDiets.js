2f90b9cb2e5e332c83588fcc70235e328bde74d6
Jonnier <jonniersalaverria@gmail.com>
2023-11-07 18:16:12 -0500

    feat(ready): controller listo

---
+++ Api/src/controllers/getAllDiets.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const axios = require('axios').default;
+const {Diets} = require("../db.js");
+const {API_KEY} = process.env;
+const cleanDiets = require("../utils/cleanDiets.js")
+const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`;
+
+
+const allDiets = async ()=>{
+    const dbDiets = await Diets.findAll()
+    if(dbDiets.length){
+        return dbDiets
+    }
+    const res = await axios.get(URL, {
+        headers: {
+            "Accept-Encoding": "null",
+        },
+    })
+    const {results} = res.data;
+    return await Diets.bulkCreate(cleanDiets(results))
+}
+
+module.exports = allDiets;
