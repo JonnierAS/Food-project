28c95affe6ea9e90ec01b2d2709572ed480c6b6c
Jonnier <jonniersalaverria@gmail.com>
2023-11-09 11:51:19 -0500

    feature(controller ready): get all Recipes from api

--- Api/src/controllers/getAllRecipe.js
+++ Api/src/controllers/getAllRecipe.js
@@ -1,10 +1,33 @@
 require('dotenv').config();
 const axios = require('axios');
+const {Recipe, Diets} = require("../db.js")
+const {API_KEY, URL_API} = process.env;
+const URL = `${URL_API}=${API_KEY}&addRecipeInformation=true`;
 
-const {API_KEY} = process.env;
 
-const getAllRecipe = ()=>{
-
+const getAllRecipe = async()=>{
+    const dbRecipe = await Recipe.findAll({
+        include: Diets
+    });
+    
+    const res = await axios.get(URL, {
+        headers: {
+            "Accept-Encoding": "null",
+        }
+    })
+    let apiRecipe = [];
+    const {results} = res.data;
+    const modifiedApiRecipe = results.map(recipe => ({
+        id: recipe.id,
+        name: recipe.title,
+        image: recipe.image,
+        summary: recipe.summary,
+        healthScore: recipe.healthScore,
+        steps: recipe.analyzedInstructions[0].steps
+    }))
+    apiRecipe.push(modifiedApiRecipe)
+    
+    return apiRecipe.concat(dbRecipe)
 }
 
 module.exports = getAllRecipe;
