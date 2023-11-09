c15070dc6385fbb1da80227fa6216697ea94bf07
Jonnier <jonniersalaverria@gmail.com>
2023-11-09 13:06:12 -0500

    feat(create): controller ready

--- Api/src/controllers/createRecipe.js
+++ Api/src/controllers/createRecipe.js
@@ -1,10 +1,36 @@
-require('dotenv').config();
-const axios = require('axios');
+const {Recipe} = require("../db.js");
+const allDiets = require("../controllers/getAllDiets.js")
 
-const {API_KEY} = process.env;
-
-const createRecipe = ()=>{
-
+const createRecipe = async(name, image, summary, healthScore, steps, diets)=>{
+    if(!name || !summary || !healthScore){
+        throw {
+            status: false,
+            message: "Falta informacion requerida"
+        }
+    }
+    const recipeExist = await Recipe.findOne({
+        where: {name}
+    })
+    if(recipeExist){
+        throw {
+            status: false,
+            message: "La receta ya existe! Selecciona otro nombre"
+        }
+    }
+    const newRecipe = await Recipe.create({
+        name, image, summary, healthScore, steps
+    })
+    const allDietsRes = await allDiets();
+    const filterDiets = diets.map(diet => (
+        allDietsRes.find(d => d.name === diet)
+    ))
+    newRecipe.addDiets(filterDiets)
+    console.log(newRecipe);
+    return{
+        status: "Done",
+        message: "Receta creada con Exito",
+        recipe: newRecipe
+    }
 }
 
 module.exports = createRecipe;
